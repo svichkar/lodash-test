1f80aa572bf27ed3711bc02e06772f171ff988fc
svichkar <2012kostyan@gmail.com>
2017-05-24 01:15:58 +0300

    done util tests

--- test/util/utilTest.js
+++ test/util/utilTest.js
@@ -20,13 +20,21 @@ describe('Util module', function () {
   });
 
   it('should return hello', function () {
-    var a = _([4,15,20, 7,3,13,20]).chain()
+    var a = _([4, 15, 20, 7, 3, 13, 20]).chain()
       .map(() => toLowerCase)
-      .filter(s =>  s !== 'o')
+      .filter(s => s !== 'o')
       .value();
     console.log(a);
   });
 
- /* var array = [4,15,20, 7,3,13,20];
-  array.filter((x) => x > 10).take(3)*/
+  var array = [4, 15, 20, 7, 3, 13, 20];
+  array;
+
+  _([4, 15, 20, 7, 3, 13, 20]).filter(x => x < 10).take(3);
+
+  _([4, 15, 20, 7, 3, 13, 20]).chain()
+    .map((v) => {console.log("map"); return v;})
+    .filter((v) => {console.log("filter"); return v < 10;})
+    .take(3)
+    .value();
 });
