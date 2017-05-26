7ecc73e6039de2a870b4e0e9647dfb62f2ab0621
svichkar <2012kostyan@gmail.com>
2017-05-26 03:21:57 +0300

    more updates

--- test/util/utilTest.js
+++ test/util/utilTest.js
@@ -2,14 +2,56 @@
  * Created by svichkar on 5/15/17.
  */
 
+const expect = require("chai").expect;
 const _util = require('lodash/util');
 const chain = require('lodash/chain');
 const map = require('lodash/map');
 const flatten = require('lodash/flatten');
 const value = require('lodash/value');
+const thru = require('lodash/thru');
 let _ = require('lodash/wrapperLodash')
 
 describe('Util module', function () {
+
+  context('#attempt(func, [args])', function () {
+
+    it('Attempts to invoke func, returning either the result or the caught error object', function () {
+
+      //given
+      function throwError() {
+        throw Error("Error message here");
+      }
+
+      //when
+      let result = _util.attempt(throwError);
+
+      //then
+      expect(result).a.instanceOf(Error);
+    });
+  });
+
+  context('#flow([funcs])', function () {
+
+    it('Should create a function that returns the result of invoking the given functions', function () {
+
+      //given
+      function sum(a, b) {
+        return a + b;
+      }
+
+      function square(v) {
+        return v * v;
+      }
+
+      let evaluate = _util.flow(sum, square);
+
+      //when
+      let result = evaluate(2, 3);
+
+      //then
+      expect(result).eq(25);
+    });
+  });
 
   it('should return hello', function () {
 
@@ -17,13 +59,13 @@ describe('Util module', function () {
       map: map,
       chain: chain,
       value: value,
-      flatten: flatten
+      flatten: flatten,
     }, false);
 
     let a = _.chain([1, 2, 3])
-      .map(x => [x, x*2])
+      .map(x => [x, x * 2])
       .flatten()
-      .value();
+      .run();
     console.log(a);
   });
 
