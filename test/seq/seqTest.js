39e98544535b75c9faf243d3e84146ea76f6c45a
Konstantin Svichkar <ksvichkar@connecture.com>
2017-05-26 14:38:35 +0300

    seq tests done

--- test/seq/seqTest.js
+++ test/seq/seqTest.js
@@ -2,12 +2,73 @@
  * Created by svichkar on 5/15/17.
  */
 
-const assert = require('chai').assert;
-const _seq = require('lodash/seq');
+const expect = require('chai').expect;
+const _ = require('lodash');
 
 describe('Seq module', function () {
-  it('should return hello', function () {
-    console.log();
-    assert.equal(2, 2);
-  })
+
+  context('#chain(value)', function () {
+
+    it('Should invoke interceptor and return value', function () {
+
+      //given
+      let users = [
+        {'user': 'barney', 'age': 36},
+        {'user': 'fred', 'age': 40},
+        {'user': 'pebbles', 'age': 1}
+      ];
+
+      //when
+      let youngest = _
+        .chain(users)
+        .sortBy('age')
+        .map(function (object) {
+          return object.user + ' is ' + object.age;
+        })
+        .head()
+        .value();
+
+      //then
+      expect(youngest).eq('pebbles is 1');
+    });
+  });
+
+  context('#tap(value, interceptor)', function () {
+
+    it('Should invoke interceptor and return value', function () {
+
+      //given
+      let array = [1, 2, 3];
+
+      //when
+      _.tap(_(array), function (array) {
+        array.pop();
+      })
+        .reverse()
+        .value();
+
+      //then
+      expect(array).eql([2, 1]);
+    });
+  });
+
+  context('#thru(value, interceptor)', function () {
+
+    it('Should invoke interceptor and return the result of interceptor', function () {
+
+      //given
+      let array = ['a', 'b', 'c', 'd'];
+
+      //when
+      let result = _(array).thru(function (array) {
+        return array.slice(0, 3);
+      })
+        .filter(v => v !== 'a')
+        .value();
+
+      //then
+      expect(result).eql(['b', 'c']);
+    });
+  });
+
 });
--- test/util/utilTest.js
+++ test/util/utilTest.js
@@ -53,20 +53,4 @@ describe('Util module', function () {
     });
   });
 
-  it('should return hello', function () {
-
-    _util.mixin(_, {
-      map: map,
-      chain: chain,
-      value: value,
-      flatten: flatten,
-    }, false);
-
-    let a = _.chain([1, 2, 3])
-      .map(x => [x, x * 2])
-      .flatten()
-      .run();
-    console.log(a);
-  });
-
 });
